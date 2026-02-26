21fd47ae68b51728de1e055db283f68a9b539517
qingchenyouforcc <qingchenformc@outlook.com>
2026-02-26 10:27:55 +0800

    feat: 添加简体中文本地化支持（zh_CN）
    在 i18n.qrc 中引入了中文翻译资源文件 shijima-qt_zh_CN.qm，并新增 shijima-qt_zh_CN.ts，提供主界面、菜单、对话框等主要功能的中文翻译，提升了中文用户的使用体验。

---
+++ translations/shijima-qt_zh_CN.ts
@@ -0,0 +1,243 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="zh_CN">
+<context>
+    <name>ShijimaManager</name>
+    <message>
+        <source>Hide</source>
+        <translation>隐藏</translation>
+    </message>
+    <message>
+        <source>Show</source>
+        <translation>显示</translation>
+    </message>
+    <message>
+        <source>Spawn</source>
+        <translation>生成</translation>
+    </message>
+    <message>
+        <source>(none)</source>
+        <translation>(无)</translation>
+    </message>
+    <message>
+        <source>Kill all</source>
+        <translation>全部关闭</translation>
+    </message>
+    <message>
+        <source>Quit</source>
+        <translation>退出</translation>
+    </message>
+    <message>
+        <source>Shijima-Qt</source>
+        <translation>Shijima-Qt</translation>
+    </message>
+    <message>
+        <source>File</source>
+        <translation>文件</translation>
+    </message>
+    <message>
+        <source>Import shimeji...</source>
+        <translation>导入看板娘...</translation>
+    </message>
+    <message>
+        <source>Show shimeji folder</source>
+        <translation>打开看板娘文件夹</translation>
+    </message>
+    <message>
+        <source>Edit</source>
+        <translation>编辑</translation>
+    </message>
+    <message>
+        <source>Delete shimeji</source>
+        <translation>删除看板娘</translation>
+    </message>
+    <message>
+        <source>Settings</source>
+        <translation>设置</translation>
+    </message>
+    <message>
+        <source>Enable multiplication</source>
+        <translation>启用繁殖</translation>
+    </message>
+    <message>
+        <source>Windowed mode</source>
+        <translation>窗口模式</translation>
+    </message>
+    <message>
+        <source>Windowed mode background...</source>
+        <translation>窗口模式背景...</translation>
+    </message>
+    <message>
+        <source>Scale</source>
+        <translation>缩放</translation>
+    </message>
+    <message>
+        <source>Custom... (%1)</source>
+        <translation>自定义... (%1)</translation>
+    </message>
+    <message>
+        <source>Save</source>
+        <translation>保存</translation>
+    </message>
+    <message>
+        <source>Language</source>
+        <translation>语言</translation>
+    </message>
+    <message>
+        <source>Help</source>
+        <translation>帮助</translation>
+    </message>
+    <message>
+        <source>View Licenses</source>
+        <translation>查看许可证</translation>
+    </message>
+    <message>
+        <source>Visit Shijima Homepage</source>
+        <translation>访问 Shijima 主页</translation>
+    </message>
+    <message>
+        <source>Report Issue</source>
+        <translation>报告问题</translation>
+    </message>
+    <message>
+        <source>Choose shimeji archive...</source>
+        <translation>选择看板娘压缩包...</translation>
+    </message>
+    <message>
+        <source>Are you sure you want to delete these shimeji?</source>
+        <translation>确定要删除这些看板娘吗？</translation>
+    </message>
+    <message>
+        <source>
+... and %1 other(s)</source>
+        <translation>
+... 以及其他 %1 个</translation>
+    </message>
+    <message>
+        <source>Cancel</source>
+        <translation>取消</translation>
+    </message>
+    <message>
+        <source>Importing shimeji...</source>
+        <translation>正在导入看板娘...</translation>
+    </message>
+    <message numerus="yes">
+        <source>Imported %n mascot(s).</source>
+        <translation>
+            <numerusform>已导入 %n 个看板娘。</numerusform>
+        </translation>
+    </message>
+    <message>
+        <source>Could not import any mascots from the specified archive(s).</source>
+        <translation>无法从指定的压缩包中导入任何看板娘。</translation>
+    </message>
+    <message>
+        <source>Import</source>
+        <translation>导入</translation>
+    </message>
+    <message>
+        <source>Welcome to Shijima! Get started by dragging and dropping a shimeji archive to the manager window. You can also import archives by selecting File &gt; Import.</source>
+        <translation>欢迎使用 Shijima！将看板娘压缩包拖放到管理器窗口即可开始。您也可以通过选择 文件 &gt; 导入 来导入压缩包。</translation>
+    </message>
+    <message>
+        <source>Close Shijima-Qt</source>
+        <translation>关闭 Shijima-Qt</translation>
+    </message>
+    <message>
+        <source>Do you want to close Shijima-Qt?</source>
+        <translation>确定要关闭 Shijima-Qt 吗？</translation>
+    </message>
+</context>
+<context>
+    <name>ShijimaContextMenu</name>
+    <message>
+        <source>Behaviors</source>
+        <translation>行为</translation>
+    </message>
+    <message>
+        <source>Show manager</source>
+        <translation>显示管理器</translation>
+    </message>
+    <message>
+        <source>Inspect</source>
+        <translation>检查</translation>
+    </message>
+    <message>
+        <source>Pause</source>
+        <translation>暂停</translation>
+    </message>
+    <message>
+        <source>Call another</source>
+        <translation>召唤同伴</translation>
+    </message>
+    <message>
+        <source>Dismiss all but one</source>
+        <translation>只保留一个</translation>
+    </message>
+    <message>
+        <source>Dismiss all</source>
+        <translation>全部解散</translation>
+    </message>
+    <message>
+        <source>Dismiss</source>
+        <translation>解散</translation>
+    </message>
+</context>
+<context>
+    <name>ShijimaLicensesDialog</name>
+    <message>
+        <source>Licenses</source>
+        <translation>许可证</translation>
+    </message>
+</context>
+<context>
+    <name>ShimejiInspectorDialog</name>
+    <message>
+        <source>Window</source>
+        <translation>窗口</translation>
+    </message>
+    <message>
+        <source>Anchor</source>
+        <translation>锚点</translation>
+    </message>
+    <message>
+        <source>Cursor</source>
+        <translation>光标</translation>
+    </message>
+    <message>
+        <source>Behavior</source>
+        <translation>行为</translation>
+    </message>
+    <message>
+        <source>Image</source>
+        <translation>图像</translation>
+    </message>
+    <message>
+        <source>Screen</source>
+        <translation>屏幕</translation>
+    </message>
+    <message>
+        <source>Work Area</source>
+        <translation>工作区</translation>
+    </message>
+    <message>
+        <source>Active IE</source>
+        <translation>活动窗口</translation>
+    </message>
+    <message>
+        <source>not visible</source>
+        <translation>不可见</translation>
+    </message>
+</context>
+<context>
+    <name>main</name>
+    <message>
+        <source>Shijima-Qt is already running!</source>
+        <translation>Shijima-Qt 已在运行！</translation>
+    </message>
+    <message>
+        <source>Shijima-Qt failed to start. Reason: </source>
+        <translation>Shijima-Qt 启动失败。原因：</translation>
+    </message>
+</context>
+</TS>
